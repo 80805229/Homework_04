2c2173e6a77816f98302ae1df2f647614f4288f5
Anna <aanikonova15@gmail.com>
2022-02-15 21:40:20 +0300

    Add Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,23 @@
+﻿// Возведите число А в натуральную степень B, используя цикл.
+
+Console.WriteLine("Введите число A ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите натуральный показатель степени");
+int b = int.Parse(Console.ReadLine());
+int a1 = 1;
+int i = 1;
+
+if (b == 0)
+{
+    a = 1;
+}
+
+while (i <= b)
+{
+    a1 = a1 * a;
+    i++;
+
+}
+
+Console.WriteLine("Результат равен " + a1);

326c4f4e03eb7f8edbbd003c472b850145ca0010
Anna <aanikonova15@gmail.com>
2022-02-15 22:55:32 +0300

    Add Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Подсчитайте сумму цифр в числе.
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (num >= 1)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+
+}
+
+Console.WriteLine("Результат равен " + sum);

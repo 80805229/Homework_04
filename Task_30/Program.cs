4486a9c63ff9454b5bd2be2646afd786a9c10c41
Anna <aanikonova15@gmail.com>
2022-02-16 21:30:20 +0300

    Add Task_30

---
+++ Task_30/Program.cs
@@ -0,0 +1,22 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+int n = 10;
+int x = 2;
+int i = 2;
+
+while (x <= n)
+{
+    if (x % 2 == 0)
+    {
+        Console.Write(x + "\t");
+    }
+    x = x + 2;
+}
+Console.WriteLine();
+
+while (i <= n)
+{
+    int i1 = i * i * i;
+    Console.Write(i1 + "\t");
+    i++;
+}

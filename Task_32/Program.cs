16dc416835c359a6f83bf85b6d8aadad6445a790
Anna <aanikonova15@gmail.com>
2022-02-17 18:45:25 +0300

    Add Task_32

---
+++ Task_32/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+
+void Mass (int[] array)
+{
+    int index = 0;
+
+    while (index < array.Length)
+    {
+        array[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] mass)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        Console.Write(mass[i] + "\t");
+    }
+}
+
+int[] array = new int [8];
+
+Mass(array);
+PrintArray(array);
